6274d66eb21eccf86bfaca60101062c876fd2180
Eugene Astafurov <eoastafurov1@gmail.com>
2019-03-18 23:04:05 +0300

    Fixed send.php

--- send.php
+++ send.php
@@ -1,26 +1,30 @@
 <?php 
+require_once "SendMailSmtpClass.php"; // подключаем класс
 
-if(isset($_POST['submit'])){
-    $to = "shikmebel_house@mail.ru"; // Здесь нужно написать e-mail, куда будут приходить письма
-    $first_name = $_POST['first_name'];
-    $subject = "УРА!!! Кто-то оставил свой телефон на сайте!";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " оставил свой номер:" . "\n\n" . $_POST['number'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
-    $headers2 = "From:" . $to;
-  
-    mail($to,$subject,$message,$headers);
-   // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
-    echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
-  echo "<br /><br /><a href='#' onclick='history.back();'>Вернуться на сайт.</a>";
+$mailSMTP = new SendMailSmtpClass('shashkovsite@gmail.com', 'shashkov', 'ssl://smtp.gmail.com', 465, "utf-8");
+// $mailSMTP = new SendMailSmtpClass('логин', 'пароль', 'хост', 'порт', 'кодировка письма');
 
-}
+$name = $_POST['first_name'];
+$phone = $_POST['number'];
+
+
+  $from = array(
+    "заявка shikmebel_house@mail.ru", // Имя отправителя
+    "shikmebel_house@mail.ru" // почта отправителя
+  );
+// кому
+
+$to = 'shikmebel_house@mail.ru';
+
+// отправляем письмо
+  $result =  $mailSMTP->send($to, 'Заявочка', $name . '<br>Номер или Email: ' . $phone . '<br>', $from);
+  // $result =  $mailSMTP->send('Кому письмо', 'Тема письма', 'Текст письма', 'Отправитель письма');
+ 
+  if($result){
+      echo "Done";
+  }else{
+     echo "Error: " . $result;
+  }
 
 ?>
-<!--Переадресация на главную страницу сайта, через 3 секунды-->
-<!--
-<script language="JavaScript" type="text/javascript">
-function changeurl(){eval(self.location="https://epicblog.net/index.php");}
-window.setTimeout("changeurl();",3000);
-</script-->
